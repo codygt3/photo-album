0bc8aa97665e1cbd899d6d2e44860fb8a5a3d12a
codygt3 <codygt@hawaii.edu>
2025-03-08 00:42:54 -1000

    added solution

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 function fix() {
-    //Write your code here!
+    let coconut = document.getElementById("coconut");
+    coconut.remove();
+    let watermelon = document.getElementById("watermelon");
+    watermelon.remove();
+    let coast = document.getElementById("coast");
+    coast.remove();
+    let sunflowers = document.getElementById("sunflowers");
+    sunflowers.remove();
+
+    let tree = document.createElement("img");
+    tree.id = "tree";
+    tree.src = "to-add/christmasTree.jpg";
+    let snowmen = document.createElement("img");
+    snowmen.id = "snowmen";
+    snowmen.src = "to-add/snowmen.jpg";
+    let stockings = document.createElement("img");
+    stockings.id = "stockings";
+    stockings.src = "to-add/stockings.jpg";
+
+    let photos = document.getElementById("photos");
+    photos.appendChild(tree);
+    photos.appendChild(snowmen);
+    photos.appendChild(stockings);
+
 }
